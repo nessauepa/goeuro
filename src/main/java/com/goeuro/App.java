a64a266162953b797aabb07d3366ee98d4025fba
Vanessa Schissato <vanessa.schissato@walmart.com>
2015-09-21 22:44:09 -0300

    Maven project created

---
+++ src/main/java/com/goeuro/App.java
@@ -0,0 +1,13 @@
+package com.goeuro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
